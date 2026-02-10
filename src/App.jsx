62e7bc2474a28bc97159e19a04c7447ebd3a4f97
piriyaluxan <user.sutheskumarpiriyaluxan025@gmail.com>
2026-02-10 10:04:07 +0530

    #1, project initialized with React and Tailwind CSS

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import { useState } from "react";
+
+function App() {
+  const [count, setCount] = useState(0);
+
+  return (
+    <>
+      <p className="font-bold text-3xl mt-10">piriyaluxan</p>
+    </>
+  );
+}
+
+export default App;
